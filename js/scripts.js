f2a126fac99981568cf8d89f25a94d8c5e9dd889
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2017-04-26 18:52:38 -0700

    improve scripts, add jazz for your soul

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-document.getElementById("audio").volume = 0.5;
+window.onload = function() {
+  var audio = document.getElementById("audio").volume = 0.15;
+  var password = document.getElementById("password")
+    , confirm_password = document.getElementById("confirm_password");
+
+  function validatePassword(){
+    if(password.value != confirmPass.value) {
+      confirmPass.setCustomValidity("Passwords Don't Match");
+    } else {
+      confirmPass.setCustomValidity('');
+    }
+  }
+
+  password.onchange = validatePassword;
+  confirmPass.onkeyup = validatePassword;
+};
